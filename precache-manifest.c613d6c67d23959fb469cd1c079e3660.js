5d897cc5b2bd17fe9218c9c23b1e8fe5420b58c4
Scenaristeur <scenaristeur@gmail.com>
2021-05-28 02:09:00 +0200

    init

---
+++ precache-manifest.c613d6c67d23959fb469cd1c079e3660.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25a961918ed73c634d4",
+    "url": "/brico/css/app.9689e1d5.css"
+  },
+  {
+    "revision": "d551d732a50bbb187359",
+    "url": "/brico/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/brico/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "aa183669b224625561eda83ff07036a5",
+    "url": "/brico/index.html"
+  },
+  {
+    "revision": "49a82a5b09f604c20fb0",
+    "url": "/brico/js/about.9da0b9dc.js"
+  },
+  {
+    "revision": "a25a961918ed73c634d4",
+    "url": "/brico/js/app.968b937f.js"
+  },
+  {
+    "revision": "f2e48fbba8c1fce166eb",
+    "url": "/brico/js/chunk-2c2ed59c.6b8ff9c5.js"
+  },
+  {
+    "revision": "2bd4dba6799f5f771c0a",
+    "url": "/brico/js/chunk-2d0b9270.0c3c4b54.js"
+  },
+  {
+    "revision": "d2ea67b0235e41868897",
+    "url": "/brico/js/chunk-2d0bceed.d7effc53.js"
+  },
+  {
+    "revision": "3b28a62e7bb249f1cd28",
+    "url": "/brico/js/chunk-33934886.0061a4dd.js"
+  },
+  {
+    "revision": "d551d732a50bbb187359",
+    "url": "/brico/js/chunk-vendors.231bedf4.js"
+  },
+  {
+    "revision": "203d55cdf7ca0ee4876976da2549efb6",
+    "url": "/brico/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/brico/robots.txt"
+  }
+]);
